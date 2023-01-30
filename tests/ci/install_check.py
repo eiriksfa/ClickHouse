b25b6d52f0548077e3cd33820c746450cfaa78c6
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2023-01-30 15:28:10 +0100

    fixup! WIP install tests

--- tests/ci/install_check.py
+++ tests/ci/install_check.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+import argparse
+import atexit
 import logging
 import os
 import sys
 import subprocess
-import atexit
+from pathlib import Path
 from typing import List, Tuple
 
 from github import Github
@@ -16,8 +18,8 @@ from clickhouse_helper import (
     prepare_tests_results_for_clickhouse,
 )
 from commit_status_helper import post_commit_status, update_mergeable_check
-from docker_pull_helper import get_image_with_version
-from env_helper import TEMP_PATH, REPORTS_PATH
+from docker_pull_helper import get_image_with_version, DockerImage
+from env_helper import TEMP_PATH as TEMP, REPORTS_PATH
 from get_robot_token import get_best_robot_token
 from pr_info import PRInfo
 from report import TestResults, TestResult
@@ -30,6 +32,77 @@ from upload_result_helper import upload_results
 
 RPM_IMAGE = "clickhouse/install-rpm-test"
 DEB_IMAGE = "clickhouse/install-deb-test"
+TEMP_PATH = Path(TEMP)
+
+
+def test_install_deb(image: DockerImage) -> TestResults:
+    tests = {
+        "Install server deb": r"""apt-get install /packages/clickhouse-{server,client,common}*deb
+systemctl start clickhouse-server
+clickhouse-client -q 'SELECT version()'""",
+        "Install keeper deb": r"""apt-get install /packages/clickhouse-keeper*deb
+systemctl start clickhouse-keeper
+for i in {1..20}; do
+    echo wait for clickhouse-keeper to being up
+    > /dev/tcp/127.0.0.1/9181 2>/dev/null || sleep 1
+done
+exec 13<>/dev/tcp/127.0.0.1/9181
+echo mntr >&13
+cat <&13 | grep zk_version""",
+    }
+    test_results = []  # type: TestResults
+    for name, command in tests.items():
+        test_results.append(test_install(name, command, image))
+
+    return test_results
+
+
+def test_install(test_name: str, command: str, image: DockerImage) -> TestResult:
+    stopwatch = Stopwatch()
+    container_name = test_name.lower().replace(" ", "_")
+    log_file = TEMP_PATH / f"{container_name}.log"
+    run_command = (
+        f"docker run --name={container_name} --detach --cap-add=SYS_PTRACE "
+        f"--rm --privileged --volume={TEMP_PATH}:/packages {image}"
+    )
+    subprocess.call(f"docker kill {container_name}", shell=True)
+    logging.info("Running docker container: `%s`", run_command)
+    subprocess.check_call(run_command, shell=True)
+    script_file = TEMP_PATH / "install.sh"
+    script_file.write_text(command)
+    install_command = f"docker exec {container_name} bash -ex /packages/install.sh"
+    with TeePopen(install_command, log_file) as process:
+        retcode = process.wait()
+        if retcode == 0:
+            status = "success"
+        else:
+            status = "failure"
+
+    subprocess.check_call(f"docker kill {container_name}", shell=True)
+    return TestResult(test_name, status, stopwatch.duration_seconds, [log_file])
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Script to release a new ClickHouse version, requires `git` and "
+        "`gh` (github-cli) commands "
+        "!!! LAUNCH IT ONLY FROM THE MASTER BRANCH !!!",
+    )
+
+    parser.add_argument(
+        "check_name",
+        help="check name, used to download the packages",
+    )
+    parser.add_argument(
+        "--no-download",
+        dest="download",
+        action="store_false",
+        default=argparse.SUPPRESS,
+        help="if set, the packages won't be downloaded, useful for debug",
+    )
+
+    return parser.parse_args()
 
 
 def main():
@@ -37,18 +110,17 @@ def main():
 
     stopwatch = Stopwatch()
 
-    check_name = sys.argv[1]
+    args = parse_args()
 
-    if not os.path.exists(TEMP_PATH):
-        os.makedirs(TEMP_PATH)
+    TEMP_PATH.mkdir(parents=True, exist_ok=True)
 
     pr_info = PRInfo()
 
     gh = Github(get_best_robot_token(), per_page=100)
 
-    atexit.register(update_mergeable_check, gh, pr_info, check_name)
+    # atexit.register(update_mergeable_check, gh, pr_info, args.check_name)
 
-    rerun_helper = RerunHelper(gh, pr_info, check_name)
+    rerun_helper = RerunHelper(gh, pr_info, args.check_name)
     if rerun_helper.is_already_finished_by_status():
         logging.info("Check is already finished according to github status, exiting")
         sys.exit(0)
@@ -58,20 +130,21 @@ def main():
         for name in (RPM_IMAGE, DEB_IMAGE)
     }
 
-    def filter_artifacts(path: str) -> bool:
-        return (
-            path.endswith(".deb")
-            or path.endswith(".rpm")
-            or path.endswith(".tgz")
-            or path.endswith("/clickhouse")
+    if args.download:
+
+        def filter_artifacts(path: str) -> bool:
+            return (
+                path.endswith(".deb")
+                or path.endswith(".rpm")
+                or path.endswith(".tgz")
+                or path.endswith("/clickhouse")
+            )
+
+        download_builds_filter(
+            args.check_name, REPORTS_PATH, TEMP_PATH, filter_artifacts
         )
 
-    download_builds_filter(check_name, REPORTS_PATH, TEMP_PATH, filter_artifacts)
-
-    run_command = (
-        f"docker run --name=test-install "
-        f"--cap-add=SYS_PTRACE --volume={REPORTS_PATH}:/packages {{docker_image}}"
-    )
+    test_reports = test_install_deb(docker_images[DEB_IMAGE])
 
     return
 
@@ -92,7 +165,7 @@ def main():
     state, description, test_results, additional_logs = process_results(test_output)
 
     ch_helper = ClickHouseHelper()
-    mark_flaky_tests(ch_helper, check_name, test_results)
+    mark_flaky_tests(ch_helper, args.check_name, test_results)
 
     report_url = upload_results(
         s3_helper,
@@ -100,10 +173,10 @@ def main():
         pr_info.sha,
         test_results,
         [run_log_path] + additional_logs,
-        check_name,
+        args.check_name,
     )
     print(f"::notice ::Report url: {report_url}")
-    post_commit_status(gh, pr_info.sha, check_name, description, state, report_url)
+    post_commit_status(gh, pr_info.sha, args.check_name, description, state, report_url)
 
     prepared_events = prepare_tests_results_for_clickhouse(
         pr_info,
@@ -112,7 +185,7 @@ def main():
         stopwatch.duration_seconds,
         stopwatch.start_time_str,
         report_url,
-        check_name,
+        args.check_name,
     )
 
     ch_helper.insert_events_into(db="default", table="checks", events=prepared_events)
