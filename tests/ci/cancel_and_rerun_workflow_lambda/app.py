9d634b5f7e1c271701cf22fb6a9e69b2850d0a6a
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2023-01-31 14:17:11 +0100

    Use new label from the event instead of all PR labels

--- tests/ci/cancel_and_rerun_workflow_lambda/app.py
+++ tests/ci/cancel_and_rerun_workflow_lambda/app.py
@@ -302,15 +302,19 @@ def main(event):
     action = event_data["action"]
     print("Got action", event_data["action"])
     pull_request = event_data["pull_request"]
-    labels = {label["name"] for label in pull_request["labels"]}
-    print("PR has labels", labels)
+    label = ""
+    if action == "labeled":
+        label = event_data["label"]["name"]
+        print("Added label:", label)
+
+    print("PR has labels", label)
     if action == "opened" or (
         action == "labeled" and pull_request["created_at"] == pull_request["updated_at"]
     ):
         print("Freshly opened PR, nothing to do")
         return
 
-    if action == "closed" or (action == "labeled" and "do not test" in labels):
+    if action == "closed" or label == "do not test":
         print("PR merged/closed or manually labeled 'do not test' will kill workflows")
         workflow_descriptions = get_workflows_description_for_pull_request(
             pull_request, token
